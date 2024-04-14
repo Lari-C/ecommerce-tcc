e50cb8d10c466870064415d695099dc51067d15c
Your Name <you@example.com>
2024-04-14 17:59:59 -0300

    add-vercel

--- frontend/components.js
+++ frontend/components.js
@@ -2,7 +2,6 @@ const menu = document.getElementById('menu-component');
 const footer = document.getElementById('footer-component');
 
 addEventListener("load", () => {
-    console.log('carregou')
     fetch('/frontend/components/menu.html').then(function (response) {
         if (response.ok) {
             return response.text();
