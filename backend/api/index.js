5f8319a354da0b3e92ace8ac36d87ab7aaeb1bc8
Your Name <you@example.com>
2024-05-07 21:55:47 -0300

    add-vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,11 @@
+const {sql} = require('@vercel/postgres');
+
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express();
-const port = 3000
+const port = 3001
 
 app.use(cors());
 
@@ -18,5 +20,14 @@ app.use('/produtos', produtoRouter);
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 })
+
+try {
+  console.log(sql)
+  const result =
+    sql`CREATE TABLE IF NOT EXISTS tb_produto ( id SERIAL, nome varchar(255), preco DECIMAL(3,2), observacao varchar(255), img varchar(255) );`;
+  console.log(result)
+} catch (error) {
+  console.log(error)
+}
 
 module.exports = app;
--- backend/api/repository/produto.js
+++ backend/api/repository/produto.js
@@ -1,36 +1,38 @@
-const db = require('../database/conection');
+//const db = require('../database/conection');
 
-const getAll = (callback) => {
-    const query = 
-    "SELECT nome, preco, observacao, img FROM tb_produto";
-    db.all(query, [], callback);
+const {sql} = require('@vercel/postgres');
+
+async function getAll() {
+    result = await sql`SELECT * FROM tb_produto;`;
+    console.log(result)
+    return result;
 }
 
 const getById = (id, callback) => {
-    const query = 
-    `SELECT nome, preco, observacao, img FROM tb_produto WHERE id = '${id}'`;
-    db.all(query, [], callback);
+    // const query = 
+    // SELECT nome, preco, observacao, img FROM tb_produto WHERE id = '${id}';
+    // db.all(query, [], callback);
 }
 
 const post = (produto, callback) => {
-    const sql = `INSERT INTO tb_produto (nome, preco, observacao, img) VALUES (?, ?, ?, ?)`;
-    db.run(sql, [produto.nome, produto.preco, produto.observacao, produto.img], (err) =>{
-        callback(err);
-    });
+    // const sql = INSERT INTO tb_produto (nome, preco, observacao, img) VALUES (?, ?, ?, ?);
+    // db.run(sql, [produto.nome, produto.preco, produto.observacao, produto.img], (err) =>{
+    //     callback(err);
+    // });
 }
 
 const remove = (id, callback) => {
-    const sql = `DELETE FROM tb_produto WHERE id = ?`;
-    db.run(sql, id, (err) =>{
-        callback(err);
-    }); 
+    // const sql = DELETE FROM tb_produto WHERE id = ?;
+    // db.run(sql, id, (err) =>{
+    //     callback(err);
+    // }); 
 }
 
 const put = (produto, callback) => {
-    const sql = `UPDATE tb_produto SET nome = ?, preco = ?, observacao = ?, img = ? WHERE id = ?`;
-    db.run(sql, [produto.nome, produto.preco, produto.observacao, produto.img, produto.id], (err) =>{
-        callback(err);
-    });
+    // const sql = UPDATE tb_produto SET nome = ?, preco = ?, observacao = ?, img = ? WHERE id = ?;
+    // db.run(sql, [produto.nome, produto.preco, produto.observacao, produto.img, produto.id], (err) =>{
+    //     callback(err);
+    // });
 }
 
 module.exports = {
