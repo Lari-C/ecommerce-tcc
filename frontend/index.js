2d9cd8433cd7370f728317e0e4031826aff3bf72
Your Name <you@example.com>
2024-06-09 19:02:58 -0300

    atualizacao-09.06

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,15 @@
 
+const API_URL = 'https://ecommerce-tcc-api-production.up.railway.app'
+
 addEventListener("load", () => {
-    fetch('http://localhost:3000/produtos/')
+    fetch(`${API_URL}/produtos`)
     .then(res => res.json())
     .then(data => {
         data.forEach(element => {
             document.getElementById('lancamentos').innerHTML += 
             `<a href="./pages/detalhes.html">
                 <div class="card">
-                    <img src="./img/${element.img}">
+                    <img src="./img/${element.card_img}">
                     <span class="sep"></span>
                     <h3>${element.nome}</h3>
                     <h4>R$ ${element.preco}</h4>
