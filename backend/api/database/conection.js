3606954b99303fc3ef90716aaf03817c70c0e7db
Your Name <you@example.com>
2024-06-12 22:49:34 -0300

    atualizacao-12.06

--- backend/api/database/conection.js
+++
@@ -1,7 +0,0 @@
-const sqlite = require('sqlite3').verbose();
-const dbName = 'ecommerce.db';
-const db = new sqlite.Database(dbName, (err) => {
-    if(err) return console.error(err);
-});
-
-module.exports = db;
